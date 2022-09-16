f88dacb011d21355d86d305e5f13de1255b50a10
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-16 17:34:41 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,85 @@
 import React from "react";
 import styled from "styled-components";
+// import EmailIcon from "@mui/icons-material/Email";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Container = styled.div`
-  background-color: black;
   height: 60px;
 `;
+
+const Wrapper = styled.div`
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+border: 1px solid lightgrey;
+display: flex;
+align-items: center;
+margin-left: 25px;
+padding 5px;`
+
+const Input = styled.input`
+border: none;
+`
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+font-weight: bold;`
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+font-size: 14px;
+cursor: pointer;
+margin-left: 25px`
+
 const Navbar = () => {
   return (
     <Container>
-      <div>navbar</div>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input>
+            </Input>
+            <SearchIcon style={{color: "grey", fontSize:16}}></SearchIcon>
+          </SearchContainer>
+        </Left>
+
+        <Center>
+          <Logo>Tebbitt Textiles </Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+          <ShoppingCartOutlinedIcon></ShoppingCartOutlinedIcon> </MenuItem>
+        </Right>
+      </Wrapper>
     </Container>
   );
 };
