59f56b3b4d4e0942128eff4d3bfa9e5082957894
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-19 16:36:34 +0200

    53 categoriesitems

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import styled from "styled-components";
+import {categories} from "../data"
+import CategoriesItem from "./CategoriesItem";
+
+const Container = styled.div``
 
 const Categories = () => {
-  return (
-    <div>Categories</div>
-  )
-}
+  return <Container>
+    {categories.map(item=> (<CategoriesItem item={item}/>))}
+  </Container>;
+};
 
-export default Categories
+export default Categories;
