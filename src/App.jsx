896ee4e69c7843fee44a20ab3f5e1df94d76e272
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-22 12:44:24 +0200

    login and register pages done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import { Home } from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import  Product  from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div>
-      <Product/>
+      <Login/>
     </div>
   );
 };
