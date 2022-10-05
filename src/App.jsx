8ad707b06a6578f179cc924883ead6c4e2181905
alextebbitt <alex.tebbitt87@gmail.com>
2022-10-05 15:00:49 +0200

    stripe

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import { Router, Routes, Route, Switch, BrowserRouter, Navigate} from "react-router-dom";
+import Success from "./pages/Success";
 
 
 
@@ -19,6 +20,7 @@ const App = () => {
       <Route path="/products/:category" element={<ProductList />} />
       <Route path="/product/:id" element={<Product />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/success" element={<Success />} />
       <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
       <Route
         path="/register"
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,7 +6,15 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { mobile } from "../responsive";
 import { useSelector } from "react-redux";
+import StripeCheckout from "react-stripe-checkout";
+import { useState } from "react";
+import { useEffect } from "react";
+import { userRequest } from "../requestMethods";
+import {useNavigate} from "react-router-dom";
 
+
+const KEY = process.env.REACT_APP_STRIPE;
+console.log("the key", KEY)
 const Container = styled.div``;
 
 const Wrapper = styled.div`
@@ -134,6 +142,27 @@ const Button = styled.button`
 `;
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
+  const [stripeToken, setStripeToken] = useState(null);
+  const navigate = useNavigate()
+
+  const onToken = (token) => {
+    setStripeToken(token);
+  };
+   useEffect(()=> {
+    const makeRequest = async ()=> {
+      try {
+        const res = await userRequest.post("/checkout/payment", {
+          tokenId: stripeToken.id,
+          amount: 500,
+        });
+          navigate("/success", {data: res.data});
+
+      } catch (err) {
+        
+      }
+    }
+    stripeToken && makeRequest()
+   }, [stripeToken, cart.total, navigate]);
   return (
     <Container>
       <Announcement />
@@ -150,8 +179,8 @@ const Cart = () => {
         </Top>
         <Bottom>
           <Info>
-            {cart.products.map((product) => (
-              <Product>
+            {cart.products.map((product, id) => (
+              <Product key={id}>
                 <ProductDetail>
                   <Image src={product.img} />
                   <Details>
@@ -195,7 +224,18 @@ const Cart = () => {
               <SummaryItemText>Total</SummaryItemText>
               <SummaryItemPrice> £ {cart.total}</SummaryItemPrice>
             </SummaryItem>
-            <Button>CHECKOUT NOW</Button>
+            <StripeCheckout
+              name="tebbitt textiles"
+              image="https://avatars.githubusercontent.com/u/1486366?v=4"
+              billingAddress
+              shippingAddress
+              description={`Your total is £${cart.total}`}
+              amount={cart.total * 100}
+              token={onToken}
+              stripeKey={KEY}
+            >
+              <Button>CHECKOUT NOW</Button>
+            </StripeCheckout>
           </Summary>
         </Bottom>
       </Wrapper>
