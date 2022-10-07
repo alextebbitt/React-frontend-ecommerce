eda7064974f4155c3fb2cf35495a8058b4d3ce28
alextebbitt <alex.tebbitt87@gmail.com>
2022-10-07 12:43:30 +0200

    persisted reducer cart

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import "./App.css";
 import React from "react";
 import { Home } from "./pages/Home";
 import ProductList from "./pages/ProductList";
-import  Product  from "./pages/Product";
+import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
-import { Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Success from "./pages/Success";
 import { useSelector } from "react-redux";
-
-
-
 
 const App = () => {
   const user = useSelector((state) => state.user.currentUser);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
-import {configureStore} from  "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
-import {persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER} from "redux-persist"
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist"
 import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
@@ -9,20 +9,17 @@ const persistConfig = {
     version: 1,
     storage,
 }
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer })
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const persistedReducer = persistReducer(persistConfig, userReducer)
-
-export const store =  configureStore({
-    reducer:{
-        cart: cartReducer,
-        user: persistedReducer,
-    },
+export const store = configureStore({
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-    }),
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
 
 export let persistor = persistStore(store)
